773b9a009aa3d0ec57fc94851d2e910f155f7fd2
TheJmqn <jman4461@gmail.com>
2023-12-15 11:01:51 -0500

    Backend transfer

--- backend/index.js
+++ backend/index.js
@@ -1,70 +1,49 @@
-import express from "express";
-import mysql from "mysql";
-import cors from "cors";
-
+const sql = require('mssql');
+const express = require('express');
+const cors = require('cors');
+const config = require('config');
 const app = express();
-const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "SQLpass",
-    database: "metronome_testing"
-});
-
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
-const songTable = "songs";
+const dbConfig = config.get('Config.dbConfig');
+const httpConfig = config.get('Config.httpConfig');
+const sqlConfig = {
+    user: dbConfig.user,
+    password: dbConfig.password,
+    server: dbConfig.server,
+    port: dbConfig.port,
+    database: dbConfig.database,
+    authentication: {
+        type: 'default'
+    },
+    options: {
+        encrypt: true
+    }
+};
 
-app.get("/data", (req, res) => {
-    const q = "SELECT * from data";
-    db.query(q, (err, data) => {
-        if (err) 
-            return res.json(err);
-        return res.json(data);
+app.post(httpConfig.uploadSong, (req, res) => {
+    console.log(req.body);
+    const q = "INSERT INTO " + dbConfig.songTable + 
+    " (title,  description, user_id, num_measures, initial_tempo, initial_time_signature, initial_key)" +
+    "VALUES ('" + req.body.title + "', '" + req.body.description + "', 1, 1, 1, 1, 0)";
+    sql.connect(sqlConfig, err => {
+        if (err) console.log("Connection error: " + err);
+        
+        const request = new sql.Request();
+        request.query(q, (err, result) => {
+            if (err) console.log("SQL Error: " + err);
+
+            res.send(result);
+        });
     });
-});
-
-app.post("/table", (req, res) => {
-    const q = "INSERT INTO data (`info`, `info2`) VALUES (?)";
-    const tableData = [
-        req.body[0].type,
-        4
-    ];
-
-    db.query(q, [tableData], (err, data) => {
-        console.log(err);
-        if (err)
-            return res.json(err);
-        return res.json("Successfully added tableData to SQL.")
-    })
-});
-
-app.post("/uploadsong", (req, res) => {
-    console.log(req);
-    const song = req.body.data;
-    const q = "INSERT INTO " + songTable + 
-    " (`title`,  `description`, `user_id`, `num_measures`, `initial_tempo`, `initial_time_signature`, `initial_key`)" +
-    "VALUES (?, ?, ?, ?, ?, ?, ?)";
-    const tableData = [
-        req.body.title, 
-        req.body.description, 
-        1, 
-        song[0].measure, 
-        req.body.tempo,
-        req.body.timeSignature,
-        0,
-    ]
-    db.query(q, tableData, (err, data) => {
-        if (err) {
-            console.log(err);
-            res.send({ success: false, message: 'query error', error: err });
-            return;
-        }
-        return res.json("Inserted song into SQL table.")
-    })
     return req.json;
 });
 
-app.listen(8080, () => {
-    console.log("Backend loaded.");
-})
+app.get(httpConfig.testing, (req, res) => {
+    res.send("Loaded.");
+});
+
+app.listen(httpConfig.port, ()=>{
+    console.log(`Application started successfully on port: ${httpConfig.port}`);
+});
